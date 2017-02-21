48e90bb1f8ea910493b42bb89584fd2d77a8e478
Anthony Najjar <anthonynsimon@gmail.com>
2017-02-21 10:43:02 +0100

    Add subcommands

--- main.go
+++ main.go
@@ -1,4 +1,25 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"os"
+
+	"github.com/google/subcommands"
+)
+
+var (
+	apiHost = flag.String("apiHost", "http://localhost:8080", "usage...")
+)
+
 func main() {
+	subcommands.Register(subcommands.HelpCommand(), "")
+	subcommands.Register(subcommands.FlagsCommand(), "")
+	subcommands.Register(subcommands.CommandsCommand(), "")
+	subcommands.Register(&convertCmd{}, "")
+	subcommands.Register(&ratesCmd{}, "")
+
+	flag.Parse()
+	ctx := context.Background()
+	os.Exit(int(subcommands.Execute(ctx)))
 }
